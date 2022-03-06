747c06d6df25769e33afc10c59370dc5eca98a76
ejagaczewski <edward.jagaczewski@gmail.com>
2022-03-05 21:45:37 -0600

    changes

--- main.tf
+++ main.tf
@@ -10,7 +10,8 @@ terraform {
 #Configure AWS Provider
 provider "aws" {
   region = "us-west-2"
-  profile = "default"
+  access_key = "AKIAR2MD64656UHZYG7K"
+  secret_key = "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"
 
     default_tags {
     tags = {
