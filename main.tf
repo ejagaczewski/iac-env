f740eb6bd8e3afbb1930db3a26f50ee60f07a60b
ejagaczewski <edward.jagaczewski@gmail.com>
2022-03-15 12:25:44 -0600

    adasd

--- main.tf
+++ main.tf
@@ -19,13 +19,3 @@ provider "aws" {
     }
     }
 }
-
-#Create VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-#Create Subnet
-resource "aws_subnet" "main" {
-    vpc_id      = aws_vpc.main.id
-    cidr_block = "10.0.1.0/24"
-}
